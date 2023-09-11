a59a23ade37b8b900310e5f8f81e4ec59ae6804d
anhquan258 <97543331+anhquan258@users.noreply.github.com>
2023-09-12 00:09:47 +0700

    feat: add backend

---
+++ MISA.Infrastructure/Context/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MISA.Infrastructure.Context
+{
+	public class DbContext :DbContext
+	{
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Host=127.0.0.1; Port=3306; Database = MISA.NTAQ; User Id = root; Password = 1232456";
+            optionsBuilder.UseMySql(connectionString);
+        }
+    }
+}
+
